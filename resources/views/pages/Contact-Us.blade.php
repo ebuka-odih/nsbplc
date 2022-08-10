ed6f8d32dd51323ee6ebf9140c1ad6a609d8bad1
Gnosis <gnosis@Gnosis-MacBook-Pro.local>
2022-08-10 17:24:36 +0100

    commit changes

--- resources/views/pages/Contact-Us.blade.php
+++ resources/views/pages/Contact-Us.blade.php
@@ -9,7 +9,7 @@
             <tbody><tr>
                 <td><h2 id="anchor-1">Customer Assistance</h2><p>At Nations Star Bank PLC, all of your banking needs can be satisfied by our friendly and professional associates at your local banking center or by reaching out to one of our Customer Service Center representatives.</p><h3>For questions regarding:</h3><ul>
                         <li>Loan and Deposit Products</li><li>Your Account</li><li>Other Bank Services</li><li>Location Hours and Accessibility</li></ul>
-                        <a href="mailto:support@nsbplc.com?subject=Customer%20Assistance%20-%20Website%20Inquiry">support@nsbplc.com</a></p>
+                        <a target="_blank" href="mailto:support@nsbplc.com?subject=Customer Assistance Website Inquiry">support@nsbplc.com</a>
                     <p>Monday – Friday: 7:00 AM – 7:00 PM<br>Saturday: 9:00 AM – 5:00 PM</p><p>Please be advised hours may vary on bank holidays.</p>
             </tr></tbody>
         </table>
